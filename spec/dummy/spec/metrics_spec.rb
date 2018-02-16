ca59fbf7b2d71091a692f7acdd729d6ab1b01573
Chris Patuzzo <chris@patuzzo.co.uk>
2018-02-16 17:22:23 +0000

    Add integration tests for the metric names

--- spec/dummy/spec/metrics_spec.rb
+++ spec/dummy/spec/metrics_spec.rb
@@ -3,4 +3,25 @@ RSpec.describe "/metrics", type: :request do
     get "/metrics"
     expect(response).to be_ok
   end
+
+  it "returns metric names from a pre-defined set" do
+    get "/metrics"
+
+    # TODO: trigger an exception
+
+    expect(unique_metric_names).to eq(%w(
+      http_server_request_duration_seconds_bucket
+      http_server_request_duration_seconds_count
+      http_server_request_duration_seconds_sum
+      http_server_requests_total
+    ))
+  end
+
+  def unique_metric_names
+    response.body
+      .split("\n")
+      .reject { |l| l.start_with?("#") }
+      .map { |l| l.split("{").first }
+      .uniq.sort
+  end
 end
